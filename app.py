d2691076ca582c23bf688ac8302addf0bf0ec9d1
Keerthi285820 <srik2301@gmail.com>
2026-05-20 21:29:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,44 @@
-import streamlit as st 
+import streamlit as st
 from questions import questions
+from evaluator import evaluate_answer
 st.title("🤖 AI Interview Simulator")
-st.write("Practice interviews and get instant feedback!")
+st.write("AI-powered interview evaluation system")
 role = st.selectbox(
     "Choose Interview Topic",
     list(questions.keys())
 )
-st.subheader(f"Interview Questions for {role}")
-score = 0
+st.subheader(f"{role} Interview")
+total_score = 0
 total_questions = len(questions[role])
 for idx, q in enumerate(questions[role]):
-    st.write(f"### Q{idx + 1}: {q['question']}")
+    st.write(f"### Q{idx+1}: {q['question']}")
     answer = st.text_area(
         f"Your Answer for Q{idx+1}",
         key=idx
     )
     if answer:
-        matched_keywords = 0
-        for keyword in q["keywords"]:
-            if keyword.lower() in answer.lower():
-                matched_keywords += 1
-        question_score = (
-            matched_keywords / len(q["keywords"]))*100
-        st.write(f"Score: {question_score:.0f}%")
-        if question_score >= 70:
+        score = evaluate_answer(
+            answer,
+            q["expected_answer"]
+        )
+        total_score += score
+        st.write(f"Similarity Score: {score:.2f}%")
+        if score >= 75:
             st.success("Excellent Answer ✅")
-            score += 1
-        elif question_score >= 40:
-            st.warning("Good attempt 👍 Add more important concepts.") 
+        elif score >= 50:
+            st.warning("Good Attempt 👍")
         else:
-            st.error("Needs Improvement🚀")    
-        st.write("Important Concepts:") 
-        st.write(",".join(q["keywords"]))      
+            st.error("Needs Improvement 🚀")
+        st.write("Expected Concept:")
+        st.info(q["expected_answer"])
 if st.button("Submit Interview"):
-    st.subheader("📊 Final Performance") 
-    final_score = (
-        score / total_questions) * 100
-    st.write(f"Final Score: {final_score:.0f}%")
+    final_score = total_score / total_questions
+    st.subheader("📊 Final Performance")
+    st.write(f"Final Score: {final_score:.2f}%")
     if final_score >= 80:
-        st.success("Outstanding Performance🎉")  
-    elif final_score >=60:
-        st.warning("Good Job! Keep Practicing👍")
+        st.success("Outstanding Performance 🎉")
+    elif final_score >= 60:
+        st.warning("Good Performance 👍")
     else:
-        st.error("Don't Worry! Practice Makes Perfect🚀")
+        st.error("Keep Practicing 🚀")
     
